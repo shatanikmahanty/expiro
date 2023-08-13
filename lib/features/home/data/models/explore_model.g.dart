51029607d4e6b01fc49ad50be4324ca352e5c3fd
Shatanik Mahanty <shatanikmahanty@gmail.com>
2023-08-13 12:28:59 +0530

    Firebase setup

---
+++ lib/features/home/data/models/explore_model.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'explore_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ExploreModel _$$_ExploreModelFromJson(Map<String, dynamic> json) =>
+    _$_ExploreModel(
+      title: json['title'] as String,
+      subtitle: json['subtitle'] as String,
+      launchUrl: json['launchUrl'] as String,
+      image: json['image'] as String,
+    );
+
+Map<String, dynamic> _$$_ExploreModelToJson(_$_ExploreModel instance) =>
+    <String, dynamic>{
+      'title': instance.title,
+      'subtitle': instance.subtitle,
+      'launchUrl': instance.launchUrl,
+      'image': instance.image,
+    };
