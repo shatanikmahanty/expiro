615ec89ae780d5608debf6752e6913b18d34a2dd
Shatanik Mahanty <shatanikmahanty@gmail.com>
2024-04-07 10:57:06 +0530

    image picker files

---
+++ lib/features/app/blocs/image_picker_cubit.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'image_picker_cubit.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_ImagePickerState _$$_ImagePickerStateFromJson(Map<String, dynamic> json) =>
+    _$_ImagePickerState(
+      pickedImagePath: json['pickedImagePath'] as String?,
+    );
+
+Map<String, dynamic> _$$_ImagePickerStateToJson(_$_ImagePickerState instance) =>
+    <String, dynamic>{
+      'pickedImagePath': instance.pickedImagePath,
+    };
